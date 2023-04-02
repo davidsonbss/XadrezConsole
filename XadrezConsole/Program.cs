b613edca8cd766ffd703862d736f08f72dee9f11
Davidson Bezerra <davidsonbss@live.com>
2023-04-02 16:23:00 -0300

    Melhorando tela.

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,14 +1,12 @@
 ﻿using TabuleiroXadrez;
 using Xadrez;
-using XadrezConsole;
+
+namespace XadrezConsole;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        PosicaoXadrez poss = new PosicaoXadrez('c', 7);
-        Console.WriteLine(poss.ToPosicao());
-
         try
         {
 
@@ -16,16 +14,17 @@ internal class Program
 
             tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
             tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
+            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 7));
+            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(3, 5));
+            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(5, 2));
 
 
             Tela.ImprimirTabuleiro(tabuleiro);
 
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
+            Console.WriteLine();
+            Posicao P = new Posicao(3, 4);
             Console.WriteLine("Posição: " + P);
+
         }
         catch (Exception ex)
         {
--- XadrezConsole/Tela.cs
+++ XadrezConsole/Tela.cs
@@ -1,19 +1,69 @@
 ﻿using TabuleiroXadrez;
+
 namespace XadrezConsole;
 class Tela
 {
+    readonly static ConsoleColor _backgroundDefault = ConsoleColor.Black;
+    readonly static ConsoleColor _CorPecaPreta = ConsoleColor.Black;
+    readonly static ConsoleColor _CorPecaBranca = ConsoleColor.White;
+
+
     public static void ImprimirTabuleiro(Tabuleiro tab)
     {
+        Console.BackgroundColor = _backgroundDefault;
+
         for (int i = 0; i < tab.Linhas; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < tab.Colunas; j++)
             {
+                ImprimirFundoTabuleiro(i, j);
                 if (tab.MovPeca(i, j) is null)
-                    Console.Write("- ");
+                    Console.Write("  ");
                 else
-                    Console.Write(tab.MovPeca(i, j) + " ");
+                {
+                    ImprimirPeca(tab.MovPeca(i, j));
+                    Console.Write(" ");
+                }
             }
+            Console.BackgroundColor = _backgroundDefault;
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h");
+    }
+
+    public static void ImprimirPeca(Peca peca)
+    {
+        ConsoleColor aux = Console.ForegroundColor;
+        if (peca.Cor == Cor.Branca)
+        {
+            Console.ForegroundColor = _CorPecaBranca;
+            Console.Write(peca);
+        }
+        else
+        {
+            Console.ForegroundColor = _CorPecaPreta;
+            Console.Write(peca);
+        }
+        Console.ForegroundColor = aux;
+    }
+
+    public static void ImprimirFundoTabuleiro(int i, int j)
+    {
+        if (i % 2 == 0)
+        {
+            if (j % 2 == 0)
+                Console.BackgroundColor = ConsoleColor.Gray;
+            else
+                Console.BackgroundColor = ConsoleColor.Green;
+        }
+
+        if (i % 2 != 0)
+        {
+            if (j % 2 == 0)
+                Console.BackgroundColor = ConsoleColor.Green;
+            else
+                Console.BackgroundColor = ConsoleColor.Gray;
         }
     }
 }
